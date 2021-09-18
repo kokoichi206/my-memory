9ac7536071517c0d11e73ce825ef7ccab048ea9f
kokoichi206 <tt.300607@gmail.com>
2021-09-18 20:55:00 +0900

    choose image intent

--- app/src/main/java/io/kokoichi/sample/mymemory/CreateActivity.kt
+++ app/src/main/java/io/kokoichi/sample/mymemory/CreateActivity.kt
@@ -1,22 +1,37 @@
 package io.kokoichi.sample.mymemory
 
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.MenuItem
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import io.kokoichi.sample.mymemory.models.BoardSize
 import io.kokoichi.sample.mymemory.utils.EXTRA_BOARD_SIZE
+import io.kokoichi.sample.mymemory.utils.isPermissionGranted
+import io.kokoichi.sample.mymemory.utils.requestPermission
 
 class CreateActivity : AppCompatActivity() {
+
+    companion object {
+        private const val TAG = "CreateActivity"
+        private const val PICK_PHOTO_CODE = 655
+        private const val READ_EXTERNAL_PHOTO_CODE = 248
+        private const val READ_PHOTOS_PERMISSION = android.Manifest.permission.READ_EXTERNAL_STORAGE
+    }
 
     private lateinit var rvImagePicker: RecyclerView
     private lateinit var etGameName: EditText
     private lateinit var btnSave: Button
 
+    private lateinit var adapter: ImagePickerAdapter
     private lateinit var boardSize: BoardSize
     private var numImagesRequired = -1
     private val chosenImageUris = mutableListOf<Uri>()
@@ -36,9 +51,49 @@ class CreateActivity : AppCompatActivity() {
         numImagesRequired = boardSize.getNumPairs()
         supportActionBar?.title = "Choose pick (0 / $numImagesRequired)"
 
-        rvImagePicker.adapter = ImagePickerAdapter(this,chosenImageUris, boardSize)
+        adapter = ImagePickerAdapter(this,chosenImageUris, boardSize, object: ImagePickerAdapter.ImageClickListener {
+            override fun onPlaceholderClicked() {
+                // 写真関連のパーミッションがあるかチェックする
+                if (isPermissionGranted(this@CreateActivity, READ_PHOTOS_PERMISSION)) {
+                    // 写真を選ぶインテントを起動
+                    launchIntentForPhotos()
+                } else {
+                    requestPermission(this@CreateActivity, READ_PHOTOS_PERMISSION, READ_EXTERNAL_PHOTO_CODE)
+                }
+            }
+        })
+        rvImagePicker.adapter = adapter
+
         rvImagePicker.setHasFixedSize(true)
         rvImagePicker.layoutManager = GridLayoutManager(this, boardSize.getWidth())
+    }
+
+    /**
+     * permission ダイアログの結果によって、行動を選択する
+     */
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        if (requestCode == READ_EXTERNAL_PHOTO_CODE) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                launchIntentForPhotos()
+            } else {
+                Toast.makeText(this, "In order to create a custom game, you need to provide access to your photos", Toast.LENGTH_LONG).show()
+            }
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
+
+    /**
+     * 写真を選択するインテント
+     */
+    private fun launchIntentForPhotos() {
+        val intent = Intent(Intent.ACTION_PICK)
+        intent.type = "image/*"
+        intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true)
+        startActivityForResult(Intent.createChooser(intent, "Choose pics"), PICK_PHOTO_CODE)
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
@@ -47,5 +102,37 @@ class CreateActivity : AppCompatActivity() {
             return true
         }
         return super.onOptionsItemSelected(item)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode != PICK_PHOTO_CODE || resultCode != Activity.RESULT_OK || data == null) {
+            Log.w(TAG, "Did not get data back from the launched activity, user likely canceled flow")
+            return
+        }
+        val selectedUri = data.data
+        val clipData = data.clipData
+        if (clipData != null) {     // 複数写真がある場合
+            Log.i(TAG, "clipData numImages ${clipData.itemCount}: $clipData")
+            for (i in 0 until clipData.itemCount) {
+                val clipItem = clipData.getItemAt(i)
+                if (chosenImageUris.size < numImagesRequired) {
+                    chosenImageUris.add(clipItem.uri)
+                }
+            }
+        } else if (selectedUri != null) {
+            Log.i(TAG, "data: $selectedUri")
+            chosenImageUris.add(selectedUri)
+        }
+        adapter.notifyDataSetChanged()
+        supportActionBar?.title = "Choose pics (${chosenImageUris} / $numImagesRequired)"
+        btnSave.isEnabled = shouldEnableSaveButton()
+    }
+
+    /**
+     * 全てのカードの写真がある＋タイトルが入力済みの時、true を返す
+     */
+    private fun shouldEnableSaveButton(): Boolean {
+        return true
     }
 }
--- app/src/main/java/io/kokoichi/sample/mymemory/ImagePickerAdapter.kt
+++ app/src/main/java/io/kokoichi/sample/mymemory/ImagePickerAdapter.kt
@@ -13,8 +13,13 @@ import kotlin.math.min
 class ImagePickerAdapter(
     private val context: Context,
     private val imageUris: List<Uri>,
-    private val boardSize: BoardSize
+    private val boardSize: BoardSize,
+    private val imageClickListener: ImageClickListener
 ) : RecyclerView.Adapter<ImagePickerAdapter.ViewHolder>() {
+
+    interface ImageClickListener {
+        fun onPlaceholderClicked()
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val view = LayoutInflater.from(context).inflate(R.layout.card_image, parent, false)
@@ -50,6 +55,7 @@ class ImagePickerAdapter(
         fun bind() {
             ivCustomImage.setOnClickListener {
                 // Launch intent for user to select photos
+                imageClickListener.onPlaceholderClicked()
             }
         }
     }
