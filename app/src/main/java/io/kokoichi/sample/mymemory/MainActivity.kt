a97a75949b9d5ea65bc6fbfdf0ef4e2e037d8f1c
kokoichi206 <tt.300607@gmail.com>
2021-09-18 17:30:50 +0900

    implement the game logic

--- app/src/main/java/io/kokoichi/sample/mymemory/MainActivity.kt
+++ app/src/main/java/io/kokoichi/sample/mymemory/MainActivity.kt
@@ -4,8 +4,11 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.coordinatorlayout.widget.CoordinatorLayout
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.snackbar.Snackbar
 import io.kokoichi.sample.mymemory.models.BoardSize
 import io.kokoichi.sample.mymemory.models.MemoryCard
 import io.kokoichi.sample.mymemory.models.MemoryGame
@@ -17,29 +20,54 @@ class MainActivity : AppCompatActivity() {
         private const val TAG = "MainActivity"
     }
 
+    private lateinit var clRoot: CoordinatorLayout
     private lateinit var rvBoard: RecyclerView
     private lateinit var tvNumMoves: TextView
     private lateinit var tvNumPairs: TextView
 
-    private var boardSize: BoardSize = BoardSize.HARD
+    private lateinit var memoryGame: MemoryGame
+    private lateinit var adapter: MemoryBoardAdapter
+    private var boardSize: BoardSize = BoardSize.EASY
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        clRoot = findViewById(R.id.clRoot)
         rvBoard = findViewById(R.id.rvBoard)
         tvNumMoves = findViewById(R.id.tvNumMoves)
         tvNumPairs = findViewById(R.id.tvNumPairs)
 
-        val memoryGame = MemoryGame(boardSize)
+        memoryGame = MemoryGame(boardSize)
 
-        rvBoard.adapter = MemoryBoardAdapter(this, boardSize, memoryGame.cards, object: MemoryBoardAdapter.CardClickListener {
+        adapter = MemoryBoardAdapter(this, boardSize, memoryGame.cards, object: MemoryBoardAdapter.CardClickListener {
             override fun onCardClicked(position: Int) {
-                Log.i(TAG, "Card clicked $position")
+                // ゲームロジック
+                updateGameWithFlip(position)
             }
 
         })
+        rvBoard.adapter = adapter
         rvBoard.setHasFixedSize(true)
         rvBoard.layoutManager = GridLayoutManager(this, boardSize.getWidth())
+    }
+
+    private fun updateGameWithFlip(position: Int) {
+        // Error checking
+        if (memoryGame.haveWonGame()) {
+            // Alert the user of an invalid move
+            Snackbar.make(clRoot, "You already won", Snackbar.LENGTH_LONG).show()
+            return
+        }
+        if (memoryGame.isCardFaceUp(position)) {
+            // Alert the user of an invalid move
+            Snackbar.make(clRoot, "Invalid move!", Snackbar.LENGTH_LONG).show()
+            return
+        }
+        // Actually flip over the card
+        if (memoryGame.flipCard(position)) {
+            Log.i(TAG, "Found a match! Num pairs found: ${memoryGame.numPairsFound}")
+        }
+        adapter.notifyDataSetChanged()
     }
 }
